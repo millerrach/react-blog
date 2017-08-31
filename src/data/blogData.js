8f2ae7fe0ac6a82a5cf7d443ad6b4d21e3e5d892
Rachel <millerrach@users.noreply.github.com>
2017-08-31 18:31:55 -0500

    A Little Cleanup

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -8,14 +8,14 @@ let blogData =
             date: 'March 16, 2017',
             author: 'Rachel',
             summary: "Welcome to our new website",
-            description: <div><span>I just finished this site using React. I had a lot of fun mocking it up and coding it into encapsulated components. I started the project using create-react-app, utilized functional programming, wrote logic in ES6 and styled it up with Sass. It's a mixture of all my favorite things! If you would like to view the code, please check out the </span><a target="_blank" href="https://github.com/millerrach/geometrics.io" className="blogLink">repository</a><span>.</span></div>
+            description: "I just finished this site using React. I had a lot of fun mocking it up and coding it into encapsulated components. I started the project using create-react-app, utilized functional programming, wrote logic in ES6 and styled it up with Sass. It's a mixture of all my favorite things!"
         },
         {
             title: 'Responsive Table',
             date: 'March 16, 2017',
             author: 'Rachel',
             summary: "Select what's important on this responsive table",
-            description: "In the coming months I will be building reusable open source React components. If you know of anything in particular the dev community needs, feel free to shoot me a message. To start it off, the table below is a responsive component that changes depending on the viewport size. As the viewport gets smaller less important items drop off. You can also use the button in the upper left hand corner to filter what you feel is most important. Feel free to test out the button in the visual below.",
+            description: "The table below is a responsive component that changes depending on the viewport size. As the viewport gets smaller less important items drop off. You can use the button in the upper right hand corner to filter what you see. Feel free to test it out.",
             visual: <ResponsiveTable />
         },
         {
@@ -30,7 +30,7 @@ let blogData =
             date: 'March 16, 2017',
             author: 'Rachel',
             summary: "They're pretty awesome",
-            description: "I was a photographer for 7 years, so I was used to limitations when trying to make things larger. So I have a some serious excitement about SVGs ability to be any size. There are so many aspects to love about them, from their mathematical base to their ability to morph into what you need. Here is a SVG that transitions on hover."
+            description: "I was a photographer for 7 years, so I was used to limitations when trying to make things larger. So I have a some serious excitement about SVGs ability to be any size. There are so many aspects to love about them, from their mathematical base to their ability to morph into what you need."
         },
         {
             title: 'Flexbox',
